1553a9e51aadfda7f11a26252bdd91af8324c932
emelonijr <emelonijr@hotmail.com>
2020-12-10 15:06:34 -0300

    V1.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title> --- Vamo que vamo! --- </title>
+</head>
+<body>
+<img src="meloni1.jpg" alt="Mãe, eu to na Globo!" title="Meloni" />
+<h1>=]~</h1> 
+</body>
+</html>
+
