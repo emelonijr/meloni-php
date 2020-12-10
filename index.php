cdba5e95388c679dc164767a3300858707499ed8
emelonijr <emelonijr@hotmail.com>
2020-12-10 15:31:13 -0300

    V1.5

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 <title> --- Vamo que vamo! --- </title>
 </head>
 <body bgcolor="f5f5dc"> 
-<font face="Georgia"> Georgia </font> <br />
 <table>
 <thead>
 <tr>
 <th><img src="meloni1.jpg" alt="Mãe, eu to na Globo!" title="Meloni" /></th>
-<th><h1>Mãe, eu to na Globo!</h1>
-<h2>=]~</h2></th>
+<th><font face="Georgia"><h1>Mãe, eu to na Globo!</h1></font>
+<font face="Georgia"><h2>=]~</h2></font></th>
 </tr>
 </tfoot>
 </table>
